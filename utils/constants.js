1d2f49e14fc3656c461fa508a8c6bfe5d3a1f292
xi-le <luke.ince@gmail.com>
2024-06-18 11:04:44 +0100

    Update constants.js
    missing, referenced in getAssets L:10

--- utils/constants.js
+++ utils/constants.js
@@ -3,3 +3,4 @@ export const privateKey = ""
 export const avgPriceFromXListed = 2
 export const discountPercentFromAvg = 10;
 export const maxListsPerCard = 1;
+export const guCardsAddress = "0xacb3c6a43d15b907e8433077b6d38ae40936fe2c"
